23c0e3bdb75c80a110d46e445325be1bc315ebe9
fatima20 <f6iim.ali@gmail.com>
2023-03-02 00:04:57 +0300

    remove job already applyed for it from home page

--- controllers/applicant.js
+++ controllers/applicant.js
@@ -9,7 +9,13 @@ exports.applicant_applyJobform_get = (req, res) => {
 //HTTP POST - send info to mangodb
 exports.applicant_applyJobform_post = (req, res) => {
     let applicant = new Applicant(req.body)
+    console.log(`hello i am the user id : ${req.body.user}`)
+    console.log(`job id  : ${req.body.job}`)
     applicant.save()
+    Job.findByIdAndUpdate(
+        { _id: req.body.job },
+        { $push: { 'applicant': req.body.user } },
+    )
         .then(() => {
             res.redirect('/applicant/viewApplyJob',)
         })
